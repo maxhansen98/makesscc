d7c766065cdc6b60bb24a75b5110fab1a6e77b4b
maxi <hansenm@cip.ifi.lmu.de>
2024-03-01 12:40:02 +0100

    SS, Local, Global still wrong

--- makesscc.py
+++ makesscc.py
@@ -3,6 +3,8 @@ import argparse
 import math
 import requests
 
+# AA 3 -> 1, glob, local, helix
+
 ATOM_TYPE_POS = (13, 16)
 POSITION = (23, 26)
 CHAIN_POS = 22
@@ -12,20 +14,45 @@ Y_COORD = (39, 46)
 Z_COORD = (47, 54)
 AA_ID = (23, 26)
 ATOM_ID = (7, 11)
+HELIX_SEQ_START = (22, 25)
+HELIX_SEQ_END = (34, 37)
+HELIX_CHAIN = 20
+SHEET_SEQ_START = (23, 26)
+SHEET_SEQ_END = (34, 37)
+SHEET_CHAIN = 22
+
+AA_DICT = {
+    "ALA": "A", "ARG": "R", "ASN": "N", "ASP": "D", "CYS": "C",
+    "GLU": "E", "GLN": "Q", "GLY": "G", "HIS": "H", "ILE": "I",
+    "LEU": "L", "LYS": "K", "MET": "M", "PHE": "F", "PRO": "P",
+    "SER": "S", "THR": "T", "TRP": "W", "TYR": "Y", "VAL": "V"
+}
 
 
-def get_atom_info(id_path, type):
+def get_file_info(id_path, type):
     """
     Parse PDB File
     Extract Atom information
     :return: Dict with the positions of the atoms as keys
     """
+    counter = 0
     atom_info = {}
+    ss_info = {'helix': [], 'sheet': []}
     with open(id_path, 'r') as pdb_file:
         for line in pdb_file:
-            if line.startswith('ATOM'):
+            if line.startswith('HELIX'):  # Get Secondary Structure-Infos for Helix
+                helix_start = int(line[HELIX_SEQ_START[0] - 1:HELIX_SEQ_START[1]].strip())
+                helix_end = int(line[HELIX_SEQ_END[0] - 1:HELIX_SEQ_END[1]].strip())
+                belonging_chain = line[HELIX_CHAIN - 1]
+                ss_info['helix'].append({'start': helix_start, 'end': helix_end, 'chain': belonging_chain})
+            if line.startswith('SHEET'):  # Get Secondary Structure-Infos for Sheet, else C
+                sheet_start = int(line[SHEET_SEQ_START[0] - 1:SHEET_SEQ_START[1]].strip())
+                sheet_end = int(line[SHEET_SEQ_END[0] - 1:SHEET_SEQ_END[1]].strip())
+                belonging_chain = line[SHEET_CHAIN - 1]
+                ss_info['sheet'].append({'start': sheet_start, 'end': sheet_end, 'chain': belonging_chain})
+            if line.startswith('ATOM'):  # Extract Atom Info
                 atom_type = line[ATOM_TYPE_POS[0] - 1:ATOM_TYPE_POS[1]].strip()
-                if atom_type == type:
+                if atom_type == type:  # For Atoms with required Atom Type
                     chain = line[CHAIN_POS - 1]
                     position = int(line[POSITION[0] - 1:POSITION[1]].strip())
                     aa = line[AA_POS[0] - 1:AA_POS[1]].strip()
@@ -36,7 +63,12 @@ def get_atom_info(id_path, type):
                     z_coord = float(line[Z_COORD[0] - 1:Z_COORD[1]])
                     # Create Atom_info Dict with Atom-Info-Lists as Values:
                     atom_info[atom_id] = (chain, position, atom_type, aa, x_coord, y_coord, z_coord)
-    return atom_info
+            if line.startswith('MODEL'):
+                counter += 1
+                if counter == 2:
+                    break
+    print(ss_info)
+    return atom_info, ss_info
 
 
 def calc_distance(atom_i, atom_j):
@@ -51,7 +83,21 @@ def calc_distance(atom_i, atom_j):
     return math.sqrt((j1 - i1) ** 2 + (j2 - i2) ** 2 + (j3 - i3) ** 2)
 
 
-def calc_contacts(atom_info, distance, seq_length):
+def get_secondary_structure(pos, chain, ss_info):
+    helix_info = ss_info['helix']
+    sheet_info = ss_info['sheet']
+
+    print(helix_info)
+    for info in helix_info:
+        if info['start'] <= int(pos) <= info['end'] and info['chain'] == chain:
+            return 'H'
+    for info in sheet_info:
+        if info['start'] <= int(pos) <= info['end'] and info['chain'] == chain:
+            return 'E'
+    return 'C'  # Coil
+
+
+def calc_contacts(atom_info, distance, seq_length, ss_info):
     """
     Calculate contacts between aas based on given distance.
     :return: Dict, containing contact info for each aa
@@ -59,12 +105,11 @@ def calc_contacts(atom_info, distance, seq_length):
     contacts = {}
     atom_id = list(atom_info.keys())
     for i, pos_i in enumerate(atom_id):
-        print("Position:", pos_i)
-        print("Atom Info:", atom_info)
         contacts[(pos_i,)] = {'chain': atom_info[pos_i][0],
                               'pos': atom_info[pos_i][1],
-                              'aa': atom_info[pos_i][3],
-                              'ss': '',
+                              'serial': pos_i,
+                              'aa': AA_DICT[atom_info[pos_i][3]],
+                              'ss': get_secondary_structure(pos_i, atom_info[pos_i][0], ss_info),
                               'global': 0,
                               'local': 0
                               }
@@ -79,8 +124,9 @@ def calc_contacts(atom_info, distance, seq_length):
             if (pos_j,) not in contacts:
                 contacts[(pos_j,)] = {'chain': atom_info[pos_j][0],
                                       'pos': atom_info[pos_j][1],
-                                      'aa': atom_info[pos_j][3],
-                                      'ss': '',
+                                      'serial': pos_j,
+                                      'aa': AA_DICT[atom_info[pos_j][3]],
+                                      'ss': get_secondary_structure(pos_j, atom_info[pos_j][0], ss_info),
                                       'global': 0,
                                       'local': 0
                                       }
@@ -108,15 +154,14 @@ def write_sscc_table(contacts, id):
         print(header)
         file.write(header)
         for key, value in contacts.items():
-            line = f"{value['chain']}\t{value['pos']}\t\t\t{value['aa']}\t{value['ss']}\t{value['global']}\t{value['local']}"
+            line = f"{value['chain']}\t{value['pos']}\t{value['serial']}\t{value['aa']}\t{value['ss']}\t{value['global']}\t{value['local']}"
             print(line)
             file.write(line)
 
 
-def generate_contact_matrix(contacts):
-    # Find unique positions
+def generate_contact_matrix():
 
-    return 0
+    return
 
 
 def main():
@@ -131,12 +176,15 @@ def main():
 
     pdb_file = get_pdb_file(args.id)
 
-    atom_info = get_atom_info(pdb_file, args.type)
-    contacts = calc_contacts(atom_info, args.distance, args.length)
+    atom_info, ss_info = get_file_info(pdb_file, args.type)
+    contacts = calc_contacts(atom_info, args.distance, args.length, ss_info)
     write_sscc_table(contacts, args.id)
 
     if args.contactmatrix:
-        contact_matrix = generate_contact_matrix(contacts, args.contractmatrix, id)
+        contact_matrix = generate_contact_matrix()
+        with open(args.contactmatrix, 'w') as f:
+            for line in contact_matrix:
+                f.write('\t'.join(map(str, line)) + '\n')
 
 
 if __name__ == '__main__':
